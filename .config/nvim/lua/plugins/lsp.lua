2736a5e89b59a6f1eae1c19d0138a55e78f57412
vivere-dally <stefanbuciujr@gmail.com>
2024-08-07 20:05:55 +0300

    mac config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,63 +1,62 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    -- LSP & Support
-    { 'neovim/nvim-lspconfig' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
-    { 'folke/neodev.nvim' },
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    dependencies = {
+        -- LSP & Support
+        { "neovim/nvim-lspconfig" },
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
+        { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+        { "folke/neodev.nvim" },
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-nvim-lua' },
-    { 'windwp/nvim-autopairs' },
-    { 'jsongerber/nvim-px-to-rem', config = true },
+        -- Autocompletion
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "hrsh7th/cmp-cmdline" },
+        { "saadparwaiz1/cmp_luasnip" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lua" },
+        { "windwp/nvim-autopairs" },
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' },
-    { 'rafamadriz/friendly-snippets' },
+        -- Snippets
+        { "L3MON4D3/LuaSnip" },
+        { "rafamadriz/friendly-snippets" },
 
-    -- Linter
-    {
-      'mfussenegger/nvim-lint',
-      lazy = true,
-      event = { 'BufReadPre', 'BufNewFile' },
+        -- Linter
+        {
+            "mfussenegger/nvim-lint",
+            lazy = true,
+            event = { "BufReadPre", "BufNewFile" },
+        },
+
+        -- Formatter
+        {
+            "stevearc/conform.nvim",
+            lazy = true,
+            event = { "BufReadPre", "BufNewFile" },
+        },
     },
+    config = function()
+        require('plugins.setup.lsp')
 
-    -- Formatter
-    {
-      'stevearc/conform.nvim',
-      lazy = true,
-      event = { 'BufReadPre', 'BufNewFile' },
-    },
-  },
-  config = function()
-    require('plugins.setup.lsp')
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                -- linters
+                "codespell",
+                "phpstan",
+                "eslint_d",
+                "pylint",
 
-    require('mason-tool-installer').setup({
-      ensure_installed = {
-        -- linters
-        'codespell',
-        'phpstan',
-        'eslint_d',
-        'pylint',
+                -- formatters
+                "php-cs-fixer",
+                "prettier",
+                "stylua",
+                "black",
+            },
+        })
 
-        -- formatters
-        'php-cs-fixer',
-        'prettier',
-        'stylua',
-        'black',
-      },
-    })
-
-    require('plugins.setup.formatter')
-    require('plugins.setup.linter')
-  end,
+        require('plugins.setup.formatter')
+        require('plugins.setup.linter')
+    end,
 }
